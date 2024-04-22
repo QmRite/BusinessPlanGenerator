70b6eed361e9295088ae4b7998cbf011b36a4c31
QmRite <lopesmigel9@gmail.com>
2024-04-22 20:50:52 +0500

    added 2 chapter logic

--- api-service/src/main/java/ru/urfu/service/impl/PlanChapterParsers/ProductDescriptionParser.java
+++ api-service/src/main/java/ru/urfu/service/impl/PlanChapterParsers/ProductDescriptionParser.java
@@ -1,11 +1,13 @@
 package ru.urfu.service.impl.PlanChapterParsers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 import ru.urfu.entity.enums.PlanChapter;
 
 import java.util.HashMap;
+import java.util.List;
 
 @Component
 public class ProductDescriptionParser extends AbstractPlanChapterParser {
@@ -17,6 +19,36 @@ public class ProductDescriptionParser extends AbstractPlanChapterParser {
     @Override
     public String getParsedContentJSON(String rawContent) {
 
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<HashMap<String, String>> rawTable = null;
+        try {
+           rawTable = mapper.readValue(
+                    rawContent, new TypeReference<List<HashMap<String, String>>>(){}
+            );
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        var header = new HashMap<String, String>();
+        header.put("problem", "Потребность (проблема)");
+        header.put("solution", "Ваше УТП (решение проблемы)");
+
+        rawTable.add(0, header);
+
+        var rawByResponse = new HashMap<String, List<HashMap<String, String>>>();
+        rawByResponse.put("Response", rawTable);
+
+        String res = null;
+        try {
+            res = new ObjectMapper().writeValueAsString(rawByResponse);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return res;
+
+/*
         var responseArray = rawContent.split("\n");
         var answersByRows = new HashMap<Integer, String[]>();
 
@@ -31,14 +63,14 @@ public class ProductDescriptionParser extends AbstractPlanChapterParser {
             answersByRows.put(i - 1, new String[] {problem, answer});
         }
 
-        String res = null;
+        String res2 = null;
         try {
-            res = new ObjectMapper().writeValueAsString(answersByRows);
+            res2 = new ObjectMapper().writeValueAsString(answersByRows);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
 
-        return res;
+        return res2;*/
     }
 
 }
--- doc-formatter/src/main/java/ru/urfu/service/impl/docServices/ProductDescriptionDocService.java
+++ doc-formatter/src/main/java/ru/urfu/service/impl/docServices/ProductDescriptionDocService.java
@@ -1,5 +1,8 @@
 package ru.urfu.service.impl.docServices;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j;
 import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -13,6 +16,9 @@ import ru.urfu.entity.enums.PlanChapter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Log4j
@@ -24,17 +30,21 @@ public class ProductDescriptionDocService extends AbstractDocService{
 
     @Override
     public byte[] createPlanChapterDoc(JSONObject content) throws InvalidFormatException, IOException {
-        HashMap<String, ArrayList<String>> rowsContentByNumber = new HashMap(content.toMap());
-        var rowsContent =  new ArrayList<>(rowsContentByNumber.values());
+        var test = content.getJSONArray("Response").toList();
+
+        ArrayList<ArrayList<String>> rowsContent2 = test.stream()
+                .map(o -> new ObjectMapper().convertValue(o, new TypeReference<Map<String, String>>() {}))
+                .map(h -> new ArrayList<>(h.values()))
+                .collect(Collectors.toCollection(ArrayList::new));
 
         XWPFDocument doc = new XWPFDocument();
-
         XWPFParagraph tmpParagraph = doc.createParagraph();
         fillTitle(tmpParagraph, "2. Описание продукта:");
         fillQuestionAndAnswer(tmpParagraph, "Какие товары (услуги) планируется реализовывать (их ключевые характеристики)", "оТВЕТ");
         fillQuestion(tmpParagraph, "В чём их ценность для потребителя (уникальное торговое предложение");
-        XWPFTable table = doc.createTable(rowsContent.size(), 2);
-        fillTable(table, rowsContent);
+
+        XWPFTable table = doc.createTable(rowsContent2.size(), 2);
+        fillTable(table, rowsContent2);
         table.setWidth("100%");
 
         ByteArrayOutputStream binOut = new ByteArrayOutputStream();
